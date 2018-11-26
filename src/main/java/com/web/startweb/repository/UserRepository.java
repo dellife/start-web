3fa7a03b64015974e9dca8aaf4a52372a8140eb6
sehee <sehee@sehee-MacBook-Pro.local>
2018-11-26 17:14:46 +0900

    [new] start-web

---
+++ src/main/java/com/web/startweb/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.web.startweb.repository;
+
+import com.web.startweb.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
